7d56e3ee0a918a26b582c0301be4c2cc679dcf75
Rachel Lew <rlew07@gmail.com>
2019-09-14 20:57:37 -0600

    Adds tests for encrypt and decrypt methods when key and date provided

---
+++ test/enigma_test.rb
@@ -0,0 +1,33 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/enigma"
+require 'date'
+
+class EnigmaTest < Minitest::Test
+
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
+
+  def test_it_encrypts_with_key_and_date
+    expected = {
+    encryption: "keder ohulw",
+    key: "02715",
+    date: "040895"
+  }
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_it_decrypts_with_key_and_date
+    expected = {
+    decryption: "hello world",
+    key: "02715",
+    date: "040895"
+  }
+    assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
+  end
+end
