498b2ed756643b229111b463a70fd4dd6d940f7d
Rachel Lew <rlew07@gmail.com>
2019-09-17 17:33:11 -0600

    Removes method to split key

--- lib/key.rb
+++ lib/key.rb
@@ -1,3 +1,5 @@
+require './lib/enigma'
+
 class Key
   attr_reader :number
 
@@ -7,15 +9,5 @@ class Key
 
   def generate_number
     rand(0..99999).to_s.rjust(5,'0')
-  end
-
-  def split_number(key = @key)
-    keys = key.split(//).each_cons(2).to_a
-    joined_keys = keys.map do |key|
-      key.join
-    end
-    joined_key_integers = joined_keys.map do |key_pairs|
-      key_pair.to_i
-    end
   end
 end
