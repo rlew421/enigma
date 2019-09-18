0da7cdd26cd050ebc8ae7cd345db898a110c1f6d
Rachel Lew <rlew07@gmail.com>
2019-09-18 05:39:44 -0600

    Removes unnecessary instance variables

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,16 +4,21 @@ class Enigma
   def initialize
     @key = Key.new
     @date = Offset.new.date
-    @encryptor = Encryptor.new(message, key, offset)
-    @message = @encryptor.message
-    @alphabet = ("a".."z").to_a << " "
   end
 
-  def encrypt(message = @message.downcase, key = @key, offset = @date)
-     {
-     encryption: @encryptor.encrypt_message(message, key, offset),
-     key: key,
-     date: offset
-     }
+  def encrypt(message = @message.downcase, key = @key, date = @date)
+      {
+      encryption: encrypt_message(message, key, date),
+      key: key,
+      date: date
+      }
+  end
+
+  def decrypt(ciphertext = @message.downcase, key = @key, date = @date)
+  {
+    decryption: decrypt_message(ciphertext, key, date),
+    key: key,
+    date: date
+  }
   end
 end
